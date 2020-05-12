3b6dd620b200ff1255efd3c3ee170fd6a10543ed
Aondongu Tivzenda <aondoe@gmail.com>
2020-05-12 19:37:11 -0400

    Changed from functional component to class based

--- todo/src/ToDoModel.js
+++ todo/src/ToDoModel.js
@@ -7,14 +7,16 @@ let ourTasks=todoList.map(tasks=> <ToDoFormat key={tasks.id} task_name={tasks.ta
 
 document.title="To Do List";
 
-function ToDoModel(){
+class ToDoModel extends React.Component{    //This is the class way form a component
 
-    return(
-        <div>
-            <h1 id="todoHead">To Do List</h1>
-                {ourTasks}                
-        </div>
-    )
+    render(){   //Render method only applies to class-formed components
+        return(
+            <div>
+                <h1 id="todoHead">To Do List</h1>
+                    {ourTasks}                
+            </div>
+        )
+    }
 }
 
 export default ToDoModel;
