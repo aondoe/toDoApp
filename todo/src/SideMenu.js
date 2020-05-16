5d0e953b3279ce98b883d09ff44a87f5399d3457
Aondongu Tivzenda <aondoe@gmail.com>
2020-05-16 13:41:41 -0400

    I added another method to the file: setZero, which will set the counter to 0.

--- todo/src/SideMenu.js
+++ todo/src/SideMenu.js
@@ -15,10 +15,11 @@ class SideMenu extends React.Component{
 
 //To use a method you created on a state, you need to bind the method, like so bellow.
     this.increment=this.increment.bind(this);
-// Here is another method below to decrement the counter number.
+// Here is another method being binded below, to decrement the counter number.
     this.decrement=this.decrement.bind(this);
-    
-    }
+// Here is another method below being binded which will set the counter to zero. 
+    this.setZero=this.setZero.bind(this);
+}
 
     // this.setState({counter:this.state.counter+1})                <!------------------This is discouraged
     increment(){    //Define a method to deal with state called increment
@@ -35,24 +36,30 @@ class SideMenu extends React.Component{
         })
     }
 
+//Below is a method to attach to the state object property: 'counter' and it will set the counter property to 0.
+    setZero(){
+        this.setState((oldState)=>{
+            return {counter: 0}
+        })
+    }
+
+
     render(){
         return (
             <div>
                 <div class="row">
                     <div class="col-lg-4"></div>
                         <div class="sideAtt bg-primary text-white">
-                            <span class="text-center text-white">
+                            <div class="form-group text-center text-white">
                                     <h1>
                                         {this.state.name} counter: {this.state.counter}
                                     </h1>
-                            </span>
-                            <br/>
                             <button onClick={this.increment}>Count Up</button>  {/*Increase the number in the counter*/}
                             <br/>
                             <button onClick={this.decrement}>Count Down</button>    {/*Decrease the number in the counter*/}
                             <br/>
-                            <br/>
-                                    {this.state.isLoggedIn?this.state.name+" is logged in": "You're not logged in"}
+                            <button onClick={this.setZero}>Set to ZERO</button>    {/*Decrease the number in the counter*/}
+                            </div>
                         </div>
                 </div>
             </div>
